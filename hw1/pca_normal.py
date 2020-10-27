b686a88731580ae4d47772948efe17664f45b438
howardyangyixuan <howardyangyixuan@gmail.com>
2020-10-27 23:11:13 +0800

    完成法向量计算，按

--- hw1/pca_normal.py
+++ hw1/pca_normal.py
@@ -16,12 +16,12 @@ import numpy as np
 def PCA(data, correlation=False, sort=True):
     # 作业1
     # 屏蔽开始
-    #1.标准化
-    x_avg = np.mean(data,axis=0)
+    # 1.标准化
+    x_avg = np.mean(data, axis=0)
     x = data - x_avg
-    #2.求协方差矩阵特征值特征向量
+    # 2.求协方差矩阵特征值特征向量
     xt = np.transpose(x)
-    h = np.matmul(xt,x)
+    h = np.matmul(xt, x)
     eigenvalues, eigenvectors = np.linalg.eig(h)
     # 屏蔽结束
 
@@ -43,8 +43,8 @@ def numpy_read_txt(pc_txt_path):
 def visualize_pc(pc, normals=None):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(pc)
-    if normals:
-        pcd.normals = normals
+    if normals is not None:
+        pcd.normals = o3d.utility.Vector3dVector(normals)
     o3d.visualization.draw_geometries([pcd])
 
 
@@ -60,7 +60,7 @@ def main():
     points = numpy_read_txt(pc_txt_path)
 
     # 显示原始点云
-    visualize_pc(points)
+    # visualize_pc(points)
 
     # 显示点数
     print('total points number is:', points.shape[0])
@@ -70,23 +70,31 @@ def main():
     point_cloud_vector = v[:, :2]  # 点云主方向对应的向量
     print('the main orientation of this pointcloud is: ', point_cloud_vector)
     # PCA
-    pca_encoder = np.matmul(points,point_cloud_vector)
-    pca_decoder = np.matmul(pca_encoder,np.transpose(point_cloud_vector))
+    pca_encoder = np.matmul(points, point_cloud_vector)
+    pca_decoder = np.matmul(pca_encoder, np.transpose(point_cloud_vector))
     # 显示PCA后结果
-    visualize_pc(pca_decoder)
+    # visualize_pc(pca_decoder)
 
     # 循环计算每个点的法向量
-    pcd_tree = o3d.geometry.KDTreeFlann(points)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd_tree = o3d.geometry.KDTreeFlann(pcd)
     normals = []
     # 作业2
     # 屏蔽开始
-
     # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数
+    for i in range(points.shape[0]):
+        [k, idx, _] = pcd_tree.search_knn_vector_3d(pcd.points[i], 20)
+        # print(idx)
+        pointsnn = np.asarray(pcd.points)[idx[1:], :]
+        eigenvalues, eigenvectors = PCA(pointsnn)
+        normal = eigenvectors[-1]
+        normals.append(normal)
 
     # 屏蔽结束
     normals = np.array(normals, dtype=np.float64)
     # TODO: 此处把法向量存放在了normals中
-    visualize_pc(points,normals)
+    visualize_pc(points, normals)
 
 
 if __name__ == '__main__':
