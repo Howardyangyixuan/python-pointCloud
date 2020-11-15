6c11d81c3dbdf1b208419be2587334082ac91436
howardyangyixuan <howardyangyixuan@gmail.com>
2020-11-15 23:41:58 +0800

    完成GMM，但有bug未调通，暂存

--- hw3/GMM.py
+++ hw3/GMM.py
@@ -3,44 +3,99 @@
 import numpy as np
 from numpy import *
 import pylab
-import random,math
+import random, math
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from scipy.stats import multivariate_normal
+
 plt.style.use('seaborn')
+
 
 class GMM(object):
     def __init__(self, n_clusters, max_iter=50):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
-    
+        self.rznk = None
+        self.pk = None
+        self.Mu = None
+        self.Var = None
+
     # 屏蔽开始
-    # 更新W
-    
+    # 更新rznk
+    def update_rznk(self, data, Mu, Var, pi):
+        n = data.shape[0]
+        print(Mu)
+        # 按类计算概率值,假设协方差矩阵为对角
+        pdfs = np.zeros((n, self.n_clusters))
+        for i in range(self.n_clusters):
+            pdfs[:, i] = pi[i] * multivariate_normal.pdf(data, Mu[i], np.diag(Var[i]))
+        rznk = pdfs / pdfs.sum(axis=1).reshape(-1, 1)
+        return rznk
 
     # 更新pi
- 
-        
-    # 更新Mu
+    # pi的和，应为1
+    def update_pk(self, rznk):
+        pk = rznk.sum(axis=0) / rznk.sum()
+        self.pk = pk
+        return pk
 
+    # Mu和Var没有约束，随机生成
+    # 更新Mu
+    def update_Mu(self, data, rznk):
+        Mu = np.zeros((self.n_clusters, data.shape[1]))
+        for i in range(self.n_clusters):
+            Mu[i] = np.average(data, axis=0, weights=rznk[:, i])
+        self.Mu = Mu
+        return Mu
 
     # 更新Var
-
+    def update_Var(self, data, Mu, rznk):
+        Var = np.zeros((self.n_clusters, data.shape[1]))
+        for i in range(self.n_clusters):
+            Var[i] = np.average((data - Mu[i]) ** 2, axis=0, weights=rznk[:, i])
+        self.Var = Var
+        return Var
 
     # 屏蔽结束
-    
+
     def fit(self, data):
         # 作业3
         # 屏蔽开始
+        # 1.初始化
+        pk = np.random.rand(self.n_clusters)
+        pk_sum = 0.0
+        for i in range(self.n_clusters):
+            pk_sum += pk[i]
+        pk /= pk_sum
+        self.pk = pk
+        Mu = np.random.rand(self.n_clusters, data.shape[1])
+        self.Mu = Mu
+        Var = np.random.rand(self.n_clusters, data.shape[1])
+        self.pk = pk
+        # 2.迭代
+        for iter in range(self.max_iter):
+            # 2.1 E step
+            rznk = self.update_rznk(data, pk, self.Mu, Var)
+            # 2.2 M step
+            self.update_Mu(data,rznk)
+            self.update_Var(data,Mu,rznk)
+            self.update_pk(rznk)
 
 
-        # 屏蔽结束
-    
+    # 屏蔽结束
+
     def predict(self, data):
-        # 屏蔽开始
+        result = []
+        rznk = self.update_rznk(data, self.pk, self.Mu, self.Var)
+        print("在各类的概率：")
+        print(rznk)
+        idx = np.argmax(rznk)
+        result.append(idx)
+# 屏蔽开始
 
-        # 屏蔽结束
+# 屏蔽结束
+
 
 # 生成仿真数据
 def generate_X(true_Mu, true_Var):
@@ -64,6 +119,7 @@ def generate_X(true_Mu, true_Var):
     plt.show()
     return X
 
+
 if __name__ == '__main__':
     # 生成数据
     true_Mu = [[0.5, 0.5], [5.5, 2.5], [1, 7]]
@@ -75,6 +131,3 @@ if __name__ == '__main__':
     cat = gmm.predict(X)
     print(cat)
     # 初始化
-
-    
-
